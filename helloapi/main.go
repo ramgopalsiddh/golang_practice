d2dbf9913a3c9a987fa923c0a063a830e412edcf
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-04-01 18:44:12 +0530

    Api for print hello world

---
+++ helloapi/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, world!\n")
+}
+
+func main() {
+	http.HandleFunc("/", helloWorld)
+	fmt.Println("Server is listening on port 8080...")
+	http.ListenAndServe(":8080", nil)
+}
