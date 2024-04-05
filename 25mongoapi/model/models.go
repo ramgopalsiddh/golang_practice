58e373a8e9c71e5bf4f0c1260658ae65c5c23e1c
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-04-05 21:49:02 +0530

    Add bson tag

--- 25mongoapi/model/models.go
+++ 25mongoapi/model/models.go
@@ -6,7 +6,7 @@ import (
 
 // struct
 type Netflix struct {
-	ID primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Movie string `json:"movie,omitempty"`
-	Watced bool `json:"watched,omitempty"`
+	ID primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"` // use omitempty for prevent defauld id [{ObjectID("000000000000000000000000")] clash
+	Movie string `json:"movie" bson:"movie"`
+	Watched bool `json:"watched" bson:"watched"`
 }
