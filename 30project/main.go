9df08298fd9f5904e7814e70670219b56f272a04
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-04-09 23:30:56 +0530

    Add age restriction in quize

--- 30project/main.go
+++ 30project/main.go
@@ -3,9 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Please Enter your name")
+	fmt.Println("Welcome in Quize, Please Enter your name")
 
 	var name string
-	fmt.Scanln(&name) // & use for memory location address refrence
-	fmt.Printf("Hello,%v welcome to quize game!", name)
+	fmt.Scan(&name) // & use for memory location address refrence
+	fmt.Printf("Hello,%v welcome to quize game!\n", name)
+
+	fmt.Printf("Enter your age: ")
+	var age uint
+	fmt.Scan(&age)
+
+	fmt.Println(age >= 10)
+
+	if age >= 10 {
+		fmt.Println("Yay you can play!")
+	} else {
+		fmt.Println("You can't play!")
+		return
+	}
+
 }
