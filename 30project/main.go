8343790f7905e7d485580f7e400ea5bc2fb56e71
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-04-11 20:23:49 +0530

    Update quize API

--- 30project/main.go
+++ 30project/main.go
@@ -22,16 +22,17 @@ func main() {
 		return
 	}
 
+	score := 0
+	num_question := 2
 
 	fmt.Printf("What is better, RTX 3080 or RTX 3090? ")
 	var answer1 string
 	var answer2 string
 	fmt.Scan(&answer1, &answer2)
 
-	if answer1 + " " + answer2 == "RTX 3090" {
+	if answer1 + " " + answer2 == "RTX 3090" || answer1 + " " + answer2 == "rtx 3090" {
 		fmt.Println("Correct !")
-	} else if answer1 + " " + answer2 == "rtx 3090" {
-		fmt.Println("Correct !")
+		score++
 	} else {
 		fmt.Println("Incorrect!")
 	}
@@ -44,7 +45,12 @@ func main() {
 
 	if cores == 12 {
 		fmt.Println("Correct!")
+		score++
 	} else {
 		fmt.Println("Incorrect!")
 	}
+
+	fmt.Printf("You Scored %v out of %v\n",score, num_question)
+	percent := (float64(score) / float64(num_question)) * 100
+	fmt.Printf("You scored: %v%%.", percent)
 }
