202301d72cd8740c87399cde8305b840d93e2dc7
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-04-15 07:29:20 +0530

    Add controller and update models

--- 32_bookstore/pkg/models/book.go
+++ 32_bookstore/pkg/models/book.go
@@ -19,3 +19,27 @@ func init(){
 	db = config.GetDB()
 	db.AutoMigrate(&Book{})
 }
+
+func (b *Book) CreatBook() *Book{
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book{
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+func GetAllBookBy(Id int64) (*Book, *gorm.DB){
+	var getBook Book
+	db := db.Where("ID=?", Id).Find(&getBook)
+	return &getBook, db
+}
+
+func DeleteBook(ID int64) Book{
+	var book Book
+	db.Where("ID=?", ID).Delete(book)
+	return book
+}
