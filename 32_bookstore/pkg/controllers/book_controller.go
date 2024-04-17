f6dc7fd447899e7f243e9ab22e6cc8da3fa350f1
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-04-17 07:38:01 +0530

    Update controller Add delete and update function

--- 32_bookstore/pkg/controllers/book_controller.go
+++ 32_bookstore/pkg/controllers/book_controller.go
@@ -43,3 +43,42 @@ func CreatBook(w http.Response, r *http.Request){
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 }
+
+func DeleteBook(w http.ResponseWriter, r *http.Request){
+	vars := mux.vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0,0)
+	if err != nil {
+		fmt.Println("error While parsing")
+	}
+	book := models.DeleteBook(ID)
+	res, _ := json.Marshal(book)
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func UpdateBook(w http.ResponseWriter, r *http.Request){
+	var updateBook = &models.Book{}
+	utils.ParseBody(r, updateBook)
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0,0)
+	if err != nil {
+		fmt.Println("Error while parsing")
+	}
+	bookDetails, db:= models.GetAllBookById(ID)
+	if updateBook.Name != ""{
+		bookDetails.Name = updateBook.Name
+	}
+	if updateBook.Author != ""{
+		bookDetails.Author = updateBook.Author
+	}
+	if updateBook.Publication != ""{
+		bookDetails.Publication = updateBook.Publication
+	}
+	db.Save(&bookDetails)
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
