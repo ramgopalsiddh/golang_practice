dcd5b2bfd11de06a855b42cb66378612c7961317
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-05-02 19:06:47 +0530

    Use psql insted of mysql as db

--- 32_bookstore/go.mod
+++ 32_bookstore/go.mod
@@ -3,8 +3,9 @@ module github.com/ramgopalsiddh/bookstore
 go 1.22.1
 
 require (
-	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	//github.com/go-sql-driver/mysql v1.5.0 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/lib/pq v1.1.1 // indirect
 )
--- 32_bookstore/pkg/config/app.go
+++ 32_bookstore/pkg/config/app.go
@@ -2,19 +2,19 @@ package config
 
 import(
 	"github.com/jinzhu/gorm"
-	//_ "github.com/jinzhu/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 ) 
 
 var (
 	db *gorm.DB
 )
 
-func Connect(){
-	d, err := gorm.Open("mysql", "ramgopal:password/tablenmae?charset=utf8&parseTime=True&loc=Local")
-	if err != nil {
-		panic(err)
-	}
-	db = d
+func Connect() {
+    d, err := gorm.Open("postgres", "host=localhost port=5432 user=ram dbname=bookmanagement sslmode=disable")
+    if err != nil {
+        panic("Failed to connect to database: " + err.Error())
+    }
+    db = d
 }
 
 func GetDB() *gorm.DB{
