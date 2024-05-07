f11a555965fb6bf40b7c3ad3b316c709e03ec86d
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-05-07 22:49:00 +0530

    Create New project slack ai chat bot

---
+++ 39_slack_ai_bot/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/shomail11/slacker"
+	//"go.mongodb.org/mongo-driver/event"
+	//"github.com/shomali11/slacker"
+	//"golang.org/x/tools/godoc/analysis"
+)
+
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent){
+	for event := range analyticsChannel{
+		fmt.Println("Command Events")
+		fmt.Println(event.Timestamp)
+		fmt.Println(event.Command)
+		fmt.Println(event.Parameters)
+		fmt.Println(event.Event)
+		fmt.Println()
+	}
+}
+
+func main() {
+	godotenv.Load(".env")
+
+	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+
+	go printCommandEvents(bot.CommandEvents())
+
+}
