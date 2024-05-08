601b7ba2a5cd358b48622b3c7968d72d87bea1e8
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-05-08 15:49:17 +0530

    Create a AI chat bot with slack, wolfram & wit-ai

--- 39_slack_ai_bot/main.go
+++ 39_slack_ai_bot/main.go
@@ -8,11 +8,13 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
-	"github.com/shomail11/slacker"
-	//"go.mongodb.org/mongo-driver/event"
-	//"github.com/shomali11/slacker"
-	//"golang.org/x/tools/godoc/analysis"
+	"github.com/krognol/go-wolfram"
+	"github.com/shomali11/slacker"
+	"github.com/tidwall/gjson"
+	witai "github.com/wit-ai/wit-go/v2"
 )
+
+var wolframClient *wolfram.Client
 
 func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent){
 	for event := range analyticsChannel{
@@ -30,6 +32,48 @@ func main() {
 
 	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
 
+	client := witai.NewClient(os.Getenv("WIT_AI_TOKEN"))
+
+	wolframClient := &wolfram.Client{AppID: os.Getenv("WOLFRAM_APP_ID")}
+
 	go printCommandEvents(bot.CommandEvents())
+
+// for bot query and answer
+	bot.Command("query for bot - <message>", &slacker.CommandDefinition{
+		Description: "send any question to wolfram",
+		//Example: "who is the president of india",
+		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter){
+			query := request.Param("message")
+			fmt.Println(query)
+			msg, _ := client.Parse(&witai.MessageRequest{
+				Query: query,
+			})
+			fmt.Println(msg)
+			// marshal message and create array
+			data, _ := json.MarshalIndent(msg, "","    ")
+			rough := string(data[:])
+			// seprate main query from array
+			value := gjson.Get(rough, "entities.wit$wolfram_search_query:wolfram_search_query.0.value")
+			fmt.Println(value)
+			// Get answer
+			answer := value.String()
+			res, err :=  wolframClient.GetSpokentAnswerQuery(answer, wolfram.Metric, 1000)
+			if err != nil {
+				fmt.Println("There is an error ")
+			}
+			// print final answer
+			response.Reply(res)
+		},
+	})
+
+
+	ctx, cancle := context.WithCancel(context.Background())
+	defer cancle()
+
+	err := bot.Listen(ctx)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 }
